e31e7ef37071b8f093943c8be881cb425132a013
suhanipm <suhanimadammanavar170503@gmail.com>
2023-11-06 13:55:12 +0530

    edited4

--- src/components/Commitment.js
+++ src/components/Commitment.js
@@ -8,7 +8,7 @@ function Commitment(){
          <div className="textnit">
             <div><p>The real catalyst, which plays important role in shaping the career of a student, is the place of study and its surroundings. Infrastructure of STJ Institute of Technology is at a serene environment on the outskirts of Ranebennur attracts one and all. The campus of the institute extends over an area of 52 acres of land, specially selected to keep the students away from the vitiated atmosphere of city life. Campus is extensively landscaped with green environment. Established in the year 1980. The college is affiliated to Visveswaraiah Technological University, Approved by All India Council for Technical Education and by the Government of Karnataka. Ranebennur is situated in Haveri District of Karnataka, on Poona Bangalore National Highway (NH-4). 295km from Bangalore and 100km from Dharwad. It can be easily reached either by Road or by Rail. Bus frequency from Bangalore to Ranebennur is very high. The Institute is situated at 3km from the Ranebennur Bus Station. The climate throughout the year is mild and salubrious.</p></div>
               <div >
-                 <img alt="" src={nitclg} className='imgnit' ></img>
+                 <img alt='stjit' src={nitclg} className='imgnit' ></img>
               </div>
         </div>
     </div>);
--- src/components/Third.js
+++ src/components/Third.js
@@ -1,7 +1,7 @@
 import clgbrown from '../Image/clgbrown1.jpg'
 function Third(){
     return( <div>
-        <img alt="" src={clgbrown} className='clgbrown'></img>
+        <img alt='' src={clgbrown} className='clgbrown'></img>
        <p className='wel'>Well-Come!!</p>
             
         </div>
